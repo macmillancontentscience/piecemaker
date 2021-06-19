363495d7172cfaa49e76f5061419d6117c3cc7c7
Jon Harmon <jonthegeek@gmail.com>
2021-06-19 17:29:57 -0500

    Add function to validate UTF8. (#1)
    * Add function to validate UTF8.
    
    Before we can do pretty much anything else, we want to have the text in a known encoding.
    
    * update rlang version dependency for when they switched the .subclass argument in `abort` to class.

--- tests/testthat/test-clean.R
+++ tests/testthat/test-clean.R
@@ -1,3 +1,40 @@
-test_that("FUNCTION works.", {
-  expect_true(FALSE)
+test_that("Can validate UTF8", {
+  non_utf8_text <- c("fa\xE7ile", "no fancy characters")
+  utf8_text <- c("façile", "no fancy characters")
+
+  expect_identical(
+    validate_utf8(utf8_text),
+    utf8_text
+  )
+  expect_identical(
+    validate_utf8(non_utf8_text),
+    utf8_text
+  )
+
+  # From ?validUTF8
+  bad_character <- "\xfa\xb4\xbf\xbf\x9f"
+
+  expect_error(
+    validate_utf8(bad_character),
+    regexp = "Unsupported string type",
+    class = "encoding_error"
+  )
+
+  # Ideally the error message should be updated to help us find where this
+  # fails.
+  mixed <- c(utf8_text, bad_character)
+  expect_error(
+    validate_utf8(mixed),
+    regexp = "Unsupported string type",
+    class = "encoding_error"
+  )
+
+  # We could also try coercing things more surgically. I don't care about that
+  # yet. Er, it seemed like it worked, but it fails when I run the test suite
+  # automatically, so I'm keeping this here to sort out.
+
+  # expect_identical(
+  #   validate_utf8(c(utf8_text, non_utf8_text)),
+  #   c(utf8_text, utf8_text)
+  # )
 })
