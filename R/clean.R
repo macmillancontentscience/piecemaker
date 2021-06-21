466b808698571ca6c42ebe5551169c859c4011fa
Jon Harmon <jonthegeek@gmail.com>
2021-06-21 18:00:53 -0500

    Add clean_text function family.
    Split into separate functions for the steps of the cleaning process, but then clean_text wraps them all.

--- R/clean.R
+++ R/clean.R
@@ -18,7 +18,7 @@
 #' convert text to UTF-8 encoding, and provides an informative error if that is
 #' not possible.
 #'
-#' @param text A character vector to attempt to convert.
+#' @param text A character vector to clean.
 #'
 #' @return The text with formal UTF-8 encoding, if possible.
 #' @export
@@ -26,10 +26,17 @@
 #' @examples
 #' validate_utf8("fa\xE7ile")
 validate_utf8 <- function(text) {
+  UseMethod("validate_utf8")
+}
+
+#' @export
+validate_utf8.character <- function(text) {
   if (all(validUTF8(text))) {
     Encoding(text) <- "UTF-8"
     return(text)
   } else {
+    # We *should* deal with multiple options here, but for now this is the one
+    # I've explicitly run into and been able to easily fix.
     converted <- iconv(text, "latin1", "UTF-8")
     if (all(converted == text)) {
       return(converted)
@@ -40,4 +47,147 @@ validate_utf8 <- function(text) {
       )
     }
   }
+}
+
+#' @export
+validate_utf8.default <- function(text) {
+  rlang::abort(
+    message = "text must be a character vector.",
+    class = "non_text_error"
+  )
+}
+
+#' Remove Non-Character Characters
+#'
+#' Unicode includes several control codes, such as \code{U+0000} (NULL, used in
+#' null-terminated strings) and \code{U+000D} (carriage return). This function
+#' removes all such characters from text.
+#'
+#' Note: We highly recommend that you first condense all space-like characters
+#' (including new lines) before removing control codes. You can easily do so
+#' with \code{\link[stringr]{str_squish}}. We also recommend validating text at
+#' the start of any cleaning process using \code{\link{validate_utf8}}.
+#'
+#' @inheritParams validate_utf8
+#'
+#' @return The character vector without control characters.
+#' @export
+#'
+#' @examples
+#' remove_control_characters("Line 1\nLine2")
+remove_control_characters <- function(text) {
+  return(
+    stringi::stri_replace_all_charclass(
+      text,
+      "\\p{C}", # This means "control character."
+      "" # Remove it.
+    )
+  )
+}
+
+#' Remove the Unicode Replacement Character
+#'
+#' The replacement character, \code{U+FFFD}, is used to mark characters that
+#' could not be loaded. These characters might be a sign of encoding issues, so
+#' it is advisable to investigate and try to eliminate any cases in your text,
+#' but in the end these characters will almost definitely confuse downstream
+#' processes.
+#'
+#' @inheritParams validate_utf8
+#'
+#' @return The character vector with replacement characters removed.
+#' @export
+#'
+#' @examples
+#' remove_replacement_characters(
+#'   paste(
+#'     "The replacement character:",
+#'     intToUtf8(65533)
+#'   )
+#' )
+remove_replacement_characters <- function(text) {
+  return(
+    stringi::stri_replace_all_regex(
+      text,
+      intToUtf8(0xfffd),
+      ""
+    )
+  )
+}
+
+#' Remove Diacritical Marks on Characters
+#'
+#' Accent characters and other diacritical marks are often difficult to type,
+#' and thus can be missing from text. To normalize the various ways a user might
+#' spell a word that should have a diacritical mark, you can convert all such
+#' characters to their simpler equivalent character.
+#'
+#' @inheritParams validate_utf8
+#'
+#' @return The character vector with simpler character representations.
+#' @export
+#'
+#' @examples
+#' remove_diacritics("façile résumé")
+remove_diacritics <- function(text) {
+  return(
+    stringi::stri_replace_all_regex(
+      stringi::stri_trans_nfd(text),
+      "\\p{Mn}",
+      ""
+    )
+  )
+}
+
+#' Title
+#'
+#' @inheritParams remove_control_characters
+#' @param whitespace Logical scalar; should we squish whitespace characters
+#'   (using \code{\link[stringr]{str_squish}})?
+#' @param control_characters Logical scalar; should we remove control
+#'   characters?
+#' @param remove_replacement_characters Logical scalar; should we remove the
+#'   "replacement character", \code{U-FFFD}?
+#' @param diacritics Logical scalar; should we remove diacritical marks
+#'   (accents, etc) from characters?
+#'
+#' @return The character vector, cleaned as specified.
+#' @export
+#'
+#' @examples
+#' example_text <- paste(
+#'   " This is a    \n\nfa\xE7ile\n\n    example.\n",
+#'   "It has the bell character, \a, and the replacement character,",
+#'   intToUtf8(65533)
+#' )
+#' clean_text(example_text)
+#' clean_text(example_text, whitespace = FALSE)
+#' clean_text(example_text, control_characters = FALSE)
+#' clean_text(example_text, replacement_characters = FALSE)
+#' clean_text(example_text, diacritics = FALSE)
+clean_text <- function(text,
+                       whitespace = TRUE,
+                       control_characters = TRUE,
+                       replacement_characters = TRUE,
+                       diacritics = TRUE) {
+  text <- validate_utf8(text)
+  if (whitespace) {
+    text <- stringr::str_squish(text)
+  }
+  if (control_characters) {
+    text <- remove_control_characters(text)
+  }
+  if (replacement_characters) {
+    text <- remove_replacement_characters(text)
+  }
+  if (diacritics) {
+    text <- remove_diacritics(text)
+  }
+
+  # Some of those processes can introduce hanging whitespace, so re-squish.
+  if (whitespace) {
+    text <- stringr::str_squish(text)
+  }
+
+  return(text)
 }
--- tests/testthat/test-clean.R
+++ tests/testthat/test-clean.R
@@ -38,3 +38,62 @@ test_that("Can validate UTF8", {
   #   c(utf8_text, utf8_text)
   # )
 })
+
+test_that("validate_utf8 fails gracefully for non-text.", {
+  expect_error(
+    validate_utf8(1L),
+    regexp = "text must be a character vector",
+    class = "non_text_error"
+  )
+  expect_error(
+    validate_utf8(TRUE),
+    regexp = "text must be a character vector",
+    class = "non_text_error"
+  )
+  expect_error(
+    validate_utf8(2.1),
+    regexp = "text must be a character vector",
+    class = "non_text_error"
+  )
+
+  # We MIGHT want to add an explicit NULL method that allows NULL through.
+  # stringi is ok with NULLs, and treats them as character(0).
+  expect_error(
+    validate_utf8(NULL),
+    regexp = "text must be a character vector",
+    class = "non_text_error"
+  )
+})
+
+test_that("clean_text cleans text.", {
+  example_text <- paste(
+    " text    \n\nfa\xE7ile\n\n    text.\n",
+    "text, \a, text,",
+    intToUtf8(65533)
+  )
+
+  expect_identical(
+    clean_text(example_text),
+    "text facile text. text, , text,"
+  )
+
+  expect_identical(
+    clean_text(example_text, whitespace = FALSE),
+    " text    facile    text. text, , text, "
+  )
+
+  expect_identical(
+    clean_text(example_text, control_characters = FALSE),
+    "text facile text. text, \a, text,"
+  )
+
+  expect_identical(
+    clean_text(example_text, replacement_characters = FALSE),
+    "text facile text. text, , text, \uFFFD"
+  )
+
+  expect_identical(
+    clean_text(example_text, diacritics = FALSE),
+    "text façile text. text, , text,"
+  )
+})
